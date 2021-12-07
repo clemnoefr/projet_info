2b3361aad055e614a23249ff89d7fb11afac4288
clemnoefr <57705783+clemnoefr@users.noreply.github.com>
2021-12-07 16:57:23 +0100

    add class files and start to write inside

---
+++ WineBiblio/Business/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WineBiblio.Business
+{
+    public class Category
+    {
+        public int id_category { get; set; }
+        public string name { get; set; }
+    }
+}
