4677ce0c67019d5ce540c3808673a7bc94bd17e1
theoduteis <theoduteis@THEODUTEISB578>
2021-12-07 15:46:41 +0100

    Add project files.

---
+++ UberWine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UberWine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ WineAPI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WineAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
