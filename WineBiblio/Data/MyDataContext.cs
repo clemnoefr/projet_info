479d991d791f6259bb46fcada8e704339b1a63fb
lolaPandaERP <86048238+lolaPandaERP@users.noreply.github.com>
2021-12-20 11:02:07 +0100

    add commentaries for MyDataContext
    Explication DbSet<TEntity> + DataContext

--- WineBiblio/Data/MyDataContext.cs
+++ WineBiblio/Data/MyDataContext.cs
@@ -3,12 +3,25 @@ using Microsoft.EntityFrameworkCore;
 
 namespace WineBiblio.Core.Data
 {
+    /*
+        Database context coordinates the functionality of the Entity Framework for our data model
+        Initializes a new instance of the DataContext class by referencing the connection used by the .NET Framework
+        Source of all entities mapped to our database connection
+    */ 
     public class MyDataContext : DbContext
     {
         public MyDataContext(DbContextOptions<MyDataContext> options) : base(options)
         {
         }
 
+        /*
+         DbSet objects are created from a DbContext using the <DbContext.Set.> method
+        For each entity in the model, we create a class that derives from DbContext
+        and contains the DbSet <TEntity> properties. 
+
+        Note that here, our DbSet <TEntity> properties have a public setter 
+        = so  they are automatically initialized when the instance of the derived context is created.
+        */
         public DbSet<Address> Address { get; set; }
         public DbSet<AddressSupplier> AddressSupplier { get; set; }
         public DbSet<Category> Category { get; set; }
